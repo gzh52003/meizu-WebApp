d76f374266ed479e34f36940e7d5b17b09784167
tikUI <1518491547@qq.com>
2020-09-04 21:04:11 +0800

    手机模块编写

--- webAPP/src/utils/request.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const request = axios.create({
-	baseURL:"http://loaclhost:2003/api",
-	withCredentials:true
-})
-
-
-export default request
--- webAPP/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    // publicPath:'/'
-    lintOnSave:'warning', // 控制eslint警告输出
-
-    devServer:{
-        compress:true, // 服务器压缩
-    }
-}
