a2c1845f5f2c58269db07356adf6ec95399ec170
tikUI <1518491547@qq.com>
2020-09-06 19:03:11 +0800

    主页模块编写和代提交登录注册更新

--- webAPP/src/views/Login.vue
+++ webAPP/src/views/Login.vue
@@ -6,69 +6,114 @@
       <van-tabbar-item style="font-size: 20px!important;">验证码登录</van-tabbar-item>
     </van-tabbar>
 
-    <van-form @submit="onSubmit">
-      <van-field 
-      name="用户名"
-      v-model="text" 
-      placeholder="手机号/ Flyme 账号" 
-      :rules="[{ required: true, message: '请填写用户名' }]"
-      maxlength="13" 
-      size="large" />
-      <van-field 
-      name="密码"
-      v-model="password" 
-      :rules="[{ required: true, message: '请填写密码' }]"
-      type="password" 
-      placeholder="密码" 
-      maxlength="12" 
-      size="large" />
-      <van-field 
-      name="验证码"
-      v-model="yzm" 
-      center 
-      clearable 
-      placeholder="请输入短信验证码" 
-      size="large">
+    <van-form @submit="onSubmit" :modul="reul">
+      <van-field
+        name="用户名"
+        v-model="reul.username"
+        placeholder="手机号/ Flyme 账号"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+        maxlength="13"
+        size="large"
+      />
+      <van-field
+        name="密码"
+        v-model="reul.password"
+        :rules="[{ required: true, message: '请填写密码' }]"
+        type="password"
+        placeholder="密码"
+        maxlength="12"
+        size="large"
+      />
+      <van-field
+        name="验证码"
+        v-model="reul.vcode"
+        :rules="[{ required: true, message: '请填写验证码' }]"
+        center
+        clearable
+        placeholder="请输入验证码"
+        maxlength="4"
+        size="large"
+      >
         <template #button>
-          <van-button 
-          size="small" 
-          type="primary">
-          发送验证码</van-button>
+          <span
+            @click="getVcode"
+            v-html="vcodeSvg"
+            class="vcode"
+            size="small"
+            type="primary"
+          ></span>
         </template>
       </van-field>
-      <van-radio-group v-model="radio">
-        <van-radio name="1" shape="square">记住登录状态</van-radio>
+      <van-checkbox-group v-model="result">
+        <van-checkbox shape="square" name="a">记住登录状态</van-checkbox>
         <span>忘记密码？</span>
-      </van-radio-group>
+      </van-checkbox-group>
       <div class="btnLogin">
-        <van-button type="primary">大号按钮</van-button>
-        <span @click="goto">注册</span>
+        <van-button @click="onLogin" type="primary">登录</van-button>
+        <span @click="gotoReg">注册</span>
       </div>
     </van-form>
   </div>
 </template>
 
 <script>
+import { Toast } from "vant";
 export default {
   name: "Login",
   data() {
     return {
+      reul: [
+        {
+          username: "",
+          password: "",
+          vcode: "",
+        },
+      ],
       active: 0,
-      text: "",
-      password: "",
-      yzm: "",
-      radio: true
+      result: ["a"],
+      vcodeSvg: "",
     };
   },
   methods: {
     onSubmit(values) {
       console.log("submit", values);
     },
-	goto(){
-		this.$router.push({
-			name:"Reg"
-		})
-	}
+    // 登录
+    async onLogin() {
+      const { data } = await this.$request.get("/login", {
+        params: {
+          ...this.reul,
+        },
+      });
+
+      if (data.code === 1) {
+        Toast.success("登录成功");
+        localStorage.setItem("currentUser", JSON.stringify(data.data));
+        this.$router.push("/home");
+      } else if (data.code === 0) {
+        Toast.fail("账号错误或者密码错误");
+      } else {
+        Toast.fail("验证码错误");
+      }
+    },
+    // 验证码
+    async getVcode() {
+      const {
+        data: { data },
+      } = await this.$request.get("/vcode");
+      this.vcodeSvg = data;
+    },
+    // 跳转注册
+    gotoReg() {
+      this.$router.push("/reg");
+    },
+  },
+  created() {
+    this.getVcode();
+    const authorization = localStorage.getItem("currentUser");
+    if (authorization) {
+      this.$router.push("/home");
+    }
   },
 };
 </script>
@@ -80,7 +125,19 @@ export default {
   margin: 20px auto;
   border: 2px solid #ccc;
 }
-.van-radio-group {
+.van-field__button{
+  position: relative;
+  .vcode{
+    position: absolute;
+    top: -20px;
+    right: -16px;
+    background: #58bc58;
+    svg{
+      width: 120px;
+    }
+  }
+}
+.van-checkbox-group {
   padding: 0 40px;
   display: flex;
   justify-content: space-between;
